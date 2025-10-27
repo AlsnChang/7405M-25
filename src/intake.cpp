623de8a0d332abd80cc8e070dea63de04c6912d0
AlsnChang <28changa@millburn.org>
2025-10-27 15:27:06 -0400

    Update intake.cpp

--- src/intake.cpp
+++ src/intake.cpp
@@ -2,9 +2,8 @@
 #include "pros/motors.hpp"
 
 
-pros::Motor secondIn (-18);
-pros::Motor firstIn (17);
-pros::Motor hopper (5);
+pros::Motor intake (19); //top & middle
+pros::Motor hopper (1); //self explanatory 
 
 pros::adi::DigitalOut hood ('H', false);
 pros::Optical opticalSensor(2);
@@ -18,77 +17,58 @@ void updateIntake()
     {
         //Put in storage
         //colorSort();
-        firstIn.move(-160);
-        secondIn.move(-160);
-        hood.set_value(true);
+        storageIn();
+        
     }
     else if (controller.get_digital(pros::E_CONTROLLER_DIGITAL_L2))
     {
         //Score in middle goal
-        firstIn.move(-160);
-        secondIn.move(160);
-        hopper.move(160);
-        hood.set_value(false);  
+        scoreMiddle();
+      
     }
     else if (controller.get_digital(pros::E_CONTROLLER_DIGITAL_L1))
     {
         //Score in long goal
-        firstIn.move(-160);
-        secondIn.move(-160);
-        hopper.move(160);
-        hood.set_value(false);
+        scoreTop();
+       
     }
     else if (controller.get_digital(pros::E_CONTROLLER_DIGITAL_R2))
     {
         //Outake into bottom goal
-        firstIn.move(160);
-        hopper.move(160); 
+        bottomGoal();       
     }
     else
     {
-        secondIn.brake();
+        intake.brake();
         hopper.brake();
-        firstIn.brake();
     }
 }
 
 void storageIn()
 {
     //Put in storage
-    firstIn.move_velocity(-200);
-    secondIn.move_velocity(-200);
-    hood.set_value(true);
+    hopper.move (-100);
+    
 }
 
 void bottomGoal()
 {
-    firstIn.move(80);
-    hopper.move(75); 
+    hopper.move(100);
+     
 }
 void stopIntake() {
-    firstIn.move(0);
-    secondIn.move(0);
-    hopper.move(0);
+    hopper.brake();
+    intake.brake();
+    
 
 }
 void scoreTop() {
-    firstIn.move(-127);
-    secondIn.move(-127);
-    hopper.move(127);
-
-    double actualVel = secondIn.get_actual_velocity();
-    if (actualVel==0) {
-        pros::lcd::print(0, "uhoh!");
-    }
-
-
+    hopper.move(100);
+    intake.move(-100);
 }
 void scoreMiddle() {
-    firstIn.move(-100);
-    secondIn.move(100);
-    hopper.move(127);
-       
-    
+    intake.move(100); 
+    hopper.move(100);
 }
 
 void colorSort()
