cc33f93023ce1f394ae8d688637039d681782763
CodingTheRobot <carissa.gulati@gmail.com>
2025-11-09 20:13:30 -0500

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -5,15 +5,15 @@
 #include "pros/motors.hpp"
 #include "lemlib/api.hpp"
 #include "autons.h"
-#include "autonSelector.h"
+#include "autonsSelector.h"
 
 // controller
 pros::Controller controller(pros::E_CONTROLLER_MASTER);
 
 // motor groups
-pros::MotorGroup leftMotors({-19, -8, 16},
+pros::MotorGroup leftMotors({-20, -18, -10},
                             pros::MotorGearset::blue); // left motor group - ports 3 (reversed), 4, 5 (reversed)
-pros::MotorGroup rightMotors({-7, 1, 13}, pros::MotorGearset::blue); // right motor group - ports 6, 7, 9 (reversed)
+pros::MotorGroup rightMotors({12, 5, 6}, pros::MotorGearset::blue); // right motor group - ports 6, 7, 9 (reversed)
 
 
 lemlib::Drivetrain drivetrain(&leftMotors, // left motor group
@@ -24,7 +24,7 @@ lemlib::Drivetrain drivetrain(&leftMotors, // left motor group
                               2 // horizontal drift is 2 (for now)
 );
 
-pros::Imu imu(3);
+pros::Imu imu(19);
 
 pros::Rotation horizontal_encoder(-15); //odom sensor
 lemlib::TrackingWheel horizontal_tracking_wheel(&horizontal_encoder, lemlib::Omniwheel::NEW_2, 0);
@@ -51,7 +51,7 @@ lemlib::ControllerSettings angular(11, // proportional gain (kP)
                                               120, // derivative gain (kD)
                                               3, // anti windup
                                               1, // small error range, in degrees
-                                              120, // small error range timeout, in milliseconds
+                                              100, // small error range timeout, in milliseconds
                                               3, // large error range, in degrees
                                               2000, // large error range timeout, in milliseconds
                                               0 // maximum acceleration (slew)
@@ -64,7 +64,7 @@ lemlib::ExpoDriveCurve steer(3, 10, 1.019);
 lemlib::Chassis chassis(drivetrain, lateral, angular, sensors, &throttle, &steer);
 
 //Scraper
-pros::adi::DigitalOut scraper ('B', false);
+pros::ADIDigitalOut scraper ('G', false);
 
 bool hoodActivated = false;
 bool scraperActivated = false;
@@ -73,23 +73,6 @@ bool scraperActivated = false;
 bool hoodPressedLast = false;
 bool scraperPressedLast = false;
 
-void coord()
-{
-    while (true)
-    {
-        lemlib::Pose pose = chassis.getPose();
-        pros::lcd::print(5, "x: %f | y: %f | Theta: %f", pose.x, pose.y);
-        pros::lcd::print(6, "Theta: %f", pose.theta);
-        if (controller.get_digital(pros::E_CONTROLLER_DIGITAL_A))
-        {
-            printf("x: %f | y: %f | Theta: %f", pose.x, pose.y);
-            printf("Theta: %f", pose.theta);
-        }
-
-        pros::delay(50);
-    }
-
-}
 // drivetrain settings
 /**
  * Runs initialization code. This occurs as soon as the program is started.
@@ -100,18 +83,14 @@ void coord()
 void initialize() {
     chassis.calibrate();
     chassis.setPose(0,0,0);
-
     pros::lcd::initialize(); // initialize brain screen
     leftMotors.set_brake_mode(pros::E_MOTOR_BRAKE_COAST);
     rightMotors.set_brake_mode(pros::E_MOTOR_BRAKE_COAST);
     opticalSensor.set_led_pwm(100);
 
     pros::delay(1000);
-    //pros::Task selector(autonSelectorStart);    
-
-
-    pros::Task updateScreen (coord);
-    pros::delay(100);
+    autonSelectorStart();
+    //pros::Task updateScreen (coord);
 }
 
 /**
@@ -134,38 +113,47 @@ void competition_initialize() {}
  */
 void autonomous() 
 {
-    topGoal();
+    startAuton();
+}
 
+void coord()
+{
+    while (true)
+    {
+        lemlib::Pose pose = chassis.getPose();
+        pros::lcd::print(5, "x: %f | y: %f | Theta: %f", pose.x, pose.y);
+        pros::lcd::print(6, "Theta: %f", pose.theta);
+        if (controller.get_digital(pros::E_CONTROLLER_DIGITAL_A))
+        {
+            printf("x: %f | y: %f | Theta: %f", pose.x, pose.y);
+            printf("Theta: %f", pose.theta);
+        }
+
+        pros::delay(50);
+    }
 
 }
 
-
-
 void opcontrol() 
 {
-    //scoreTop();
-    //topGoalSide();
-    //bottomGoalSide();
-
-
     while (true)
     {
         int leftY = controller.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y);
         int rightX = controller.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_X);
 
 		chassis.arcade(leftY, rightX);
-
+/*
         bool hoodPressedNow = controller.get_digital(pros::E_CONTROLLER_DIGITAL_A);
 
         if (hoodPressedNow && !hoodPressedLast) {
             // Toggle hood
             hoodActivated = !hoodActivated;
             hood.set_value(hoodActivated);
-        }
+        } 
 
-        hoodPressedLast = hoodPressedNow;
+        hoodPressedLast = hoodPressedNow; */
 
-        bool scraperPressedNow = controller.get_digital(pros::E_CONTROLLER_DIGITAL_B);
+        bool scraperPressedNow = controller.get_digital(pros::E_CONTROLLER_DIGITAL_A);
 
         if (scraperPressedNow && !scraperPressedLast) {
             // Toggle hood
@@ -178,7 +166,4 @@ void opcontrol()
         updateIntake();
         pros::delay(20);
     }
-        
-        
-
 }
