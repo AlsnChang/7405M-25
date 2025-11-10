20124fc11ced176ce4f3282adad3600cacac6fb7
CodingTheRobot <carissa.gulati@gmail.com>
2025-11-09 20:14:02 -0500

    Update auton.cpp

--- src/auton.cpp
+++ src/auton.cpp
@@ -1,6 +1,6 @@
 #include "intake.h"
 #include "pros/motors.hpp"
-#include "autonSelector.h"
+#include "autonsSelector.h"
 #include "main.h"
 #include "pros/adi.hpp"
 #include "pros/misc.h"
@@ -9,109 +9,55 @@
 
 void bottomGoalSide() {
     //Align with three blocks
-    chassis.moveToPoint(0, 11, 900);
+    chassis.moveToPoint(0, 11, 1000);
     //50
-    chassis.turnToPoint(13.4, 26.1, 800, {.maxSpeed=80});
+    chassis.turnToPoint(13.4, 26.1, 1000, {.maxSpeed=80});
     storageIn();
     //Get blocks
     // chassis.moveToPoint(5.52, 18.7, 1000, {.maxSpeed = 25});
-    chassis.moveToPoint(13.4, 26.1, 2000, {.maxSpeed = 25});
+    chassis.moveToPoint(13.4, 26.1, 3000, {.maxSpeed = 25});
     chassis.turnToPoint(5.5,35.5, 1000, {.maxSpeed = 80});
 
-    chassis.moveToPoint(4.23,35.53,1000);
-    //pros::delay(40);
+    chassis.moveToPoint(5.5,35.5,5000);
     bottomGoal();
-    pros::delay(1800);
-    hopper.move(-127);
-    pros::delay(500);
-    hopper.move(0);
-    storageIn();
-    hopper.move(0);
+    pros::delay(2000);
+
     
-    chassis.moveToPoint(39.5, 2.61, 1500, {.forwards = false, .maxSpeed = 67});
-
-    chassis.turnToHeading(-180, 600, {.maxSpeed = 70});
-
-    scraper.set_value(true);
-    chassis.moveToPoint(40.4, -9.5, 2000,{.maxSpeed=90});
-    pros::delay(100);
-
-    // //move to long goal
-    chassis.moveToPoint(39.5, 3,1000, {.forwards = false});
-    scraper.set_value(false);
-    chassis.turnToHeading(0,1000, {.maxSpeed = 70});
-    pros::delay(900);
-    hood.set_value(hoodActivated);
-    chassis.moveToPoint(40.15, 15.75,900, {.forwards = true});
-    scoreTop();
-
-
-}
-void topGoal() {
-    //Align with three blocks
-    chassis.moveToPoint(0, 11, 900);
-    //50
-    chassis.turnToPoint(-13.4, 26.1, 800, {.maxSpeed=80});
-    storageIn();
-    //Get blocks
-    chassis.moveToPoint(-13.4, 26.1, 2000, {.maxSpeed = 25});
-    chassis.turnToPoint(-5.5, 35.5, 1000, {.maxSpeed = 70});
-
-    //move towards center goal
-    scraper.set_value(true);
-    chassis.moveToPoint(-0.5, 36.53, 1000);
-    pros::delay(100);
-    scoreMiddle();
-    pros::delay(1500);
+   //loader
     hopper.move(-127);
-    pros::delay(500);
-    hopper.move(0);
+    stopIntake();
+    chassis.moveToPoint(39, 2.61, 2500, {.forwards = false, .maxSpeed = 80});
+
+    chassis.turnToHeading(-180, 750, {.maxSpeed = 90});
     storageIn();
-    hopper.move(0);
-
-    chassis.moveToPoint(-39.5, 2.61, 1500, {.forwards = false, .maxSpeed = 80});
-
-    chassis.turnToHeading(180, 600, {.maxSpeed = 70}); // -180 mirrored → 180
 
     scraper.set_value(true);
-    chassis.moveToPoint(-39.5, -11.75, 1200, {.maxSpeed=80});
-    pros::delay(50);
+    chassis.moveToPoint(39, -11, 2000,{.minSpeed=100});
+    pros::delay(1500);
 
     //move to long goal
-    // chassis.moveToPoint(-39.5, 10, 1000, {.forwards = false});
-    chassis.moveToPoint(-39.5, 4, 700, {.forwards = false, .maxSpeed = 90});
-
+    chassis.moveToPoint(39, 10,2000, {.forwards = false});
     scraper.set_value(false);
-    chassis.turnToHeading(0, 1000, {.maxSpeed = 70}); // stays the same
-    pros::delay(800);
-    hood.set_value(hoodActivated);
-    chassis.moveToPoint(-39.5, 15.75, 900, {.forwards = true});
-    pros::delay(200);
-    scoreTop();
-    
+    chassis.turnToHeading(0,1000, {.maxSpeed = 90});
+    pros::delay(1000);
+    chassis.moveToPoint(39, 15.5,5000, {.forwards = true});
+    //scoreTop();
+
 }
 
+void topGoalSide()
+{
 
+}
 
-void startAuton() {
-    int auton = getSelectedAuton();
-
-    if (auton == 0) {
+void startAuton()
+{
+    if (getSelectedAuton() == 0 || getSelectedAuton() == 4)
+    {
         bottomGoalSide();
     }
-    else if (auton ==1) {
-        topGoal();
+    else if (getSelectedAuton() == 5 || getSelectedAuton() == 1)
+    {
+        topGoalSide();
     }
-
-    else if (auton == 4) {
-        bottomGoalSide(); 
-    }
-    else if (auton ==5) {
-        topGoal();
-    }
-    else {
-        pros::delay(1);
-    }
-    
-    
 }
